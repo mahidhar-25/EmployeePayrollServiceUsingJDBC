f2e6d04bffbbb2d3cce862c0fae7be7a0a7d9fd8
mahidhar reddy <mahidhar100008@gmail.com>
2023-12-11 20:29:51 +0530

    Added : initialized the employee payroll service using jdbc

---
+++ src/com/bridgelabz/employeepayrollservice/Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayrollservice;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("!!! welcome to employee payroll service using jdbc !!!");
+    }
+}
