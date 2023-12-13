951c5f03a37edc2519fc3871922582dfc3048423
mahidhar reddy <mahidhar100008@gmail.com>
2023-12-13 05:57:05 +0530

    Added : refactored code to do with prepared statement

--- test/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
+++ test/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.employeepayrollservice;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -7,13 +8,16 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class EmployeePayrollServiceTest {
-
+    private EmployeePayrollService employeePayrollService;
+    @Before
+    public void setup(){
+       employeePayrollService = new EmployeePayrollService();
+    }
     /*
     @desc : this test is to check the connectivity and initially the count of the from database is matching or not
      */
     @Test
     public void readEmployeeDetails_FromJavaProgramAnd_CountShouldMatchWithEmployeeCount() {
-        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
         ArrayList<EmployeePayrollData> employeeData = employeePayrollService.readEmployeeDetails();
         assertEquals(3 , employeeData.size());
     }
@@ -23,8 +27,16 @@ public class EmployeePayrollServiceTest {
      */
     @Test
     public void updateEmployeeSalaryInDatabaseAndCheckWhetherTheyAreInSync() {
-        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
         employeePayrollService.updateEmployeeSalaryInDatabase("Karthik" , 300000);
+        boolean isDataInSync = employeePayrollService.checkEmployeePayrollIsInSyncWithDatabase("Karthik");
+        assertTrue(isDataInSync);
+    }
+    /*
+ @desc : whenever we up[date whether it is in sync with the database or not
+  */
+    @Test
+    public void updateEmployeeSalaryInDatabaseUsingPreparedStatementAndCheckWhetherTheyAreInSync() {
+        employeePayrollService.updateEmployeeSalaryInDatabaseUsingPreparedStatement("Karthik" , 30000);
         boolean isDataInSync = employeePayrollService.checkEmployeePayrollIsInSyncWithDatabase("Karthik");
         assertTrue(isDataInSync);
     }
